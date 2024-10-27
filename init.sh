5183ade21711e98c0dd9452dbe79b34c2a2de082
Matan Maimon <mataname@gmail.com>
2024-10-27 20:57:26 +0200

    ..

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ echo "Clearing contents of $TARGET_WP_DIR..."
 rm -rf "${TARGET_WP_DIR:?}"/*
 
 # Copy fresh files from wp-app
-echo "Copying files from wp-app to WordPress directory..."
-cp -r /wp-app/* "$TARGET_WP_DIR/"
+# echo "Copying files from wp-app to WordPress directory..."
+# cp -r /wp-app/* "$TARGET_WP_DIR/"
 
 echo "WordPress directory reset complete."
