10524612f2ac5a593f7efec9436110e7f5f6b04d
Matan Maimon <mataname@gmail.com>
2024-10-27 21:30:43 +0200

    ..

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Define source and target directories
-SOURCE_DIR="/wp-app"
-TARGET_DIR="../files/wp-app"
+SOURCE_DIR="/wp_app"
+TARGET_DIR="../files/wp_app"
 
 # Check if the source directory exists
 if [ -d "$SOURCE_DIR" ]; then
     # Ensure target directory structure exists
     mkdir -p "$TARGET_DIR"
 
-    # Copy the contents of wp-app to the target directory
+    # Copy the contents of wp_app to the target directory
     echo "Copying $SOURCE_DIR to $TARGET_DIR..."
     cp -r "$SOURCE_DIR"/* "$TARGET_DIR/"
     
@@ -17,7 +17,7 @@ if [ -d "$SOURCE_DIR" ]; then
     # echo "Removing original files from $SOURCE_DIR..."
     # rm -rf "$SOURCE_DIR"/*
 
-    echo "Move complete. All contents from wp-app have been copied to $TARGET_DIR and original source cleared."
+    echo "Move complete. All contents from wp_app have been copied to $TARGET_DIR and original source cleared."
 else
     echo "Source directory $SOURCE_DIR does not exist. Skipping move."
 fi
